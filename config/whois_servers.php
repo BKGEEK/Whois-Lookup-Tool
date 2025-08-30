814278a0016e247c4754e0aef8bee7bba79febee
BKGEEK <163123862+BKGEEK@users.noreply.github.com>
2025-08-30 12:03:24 +0800

    Add files via upload

---
+++ config/whois_servers.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'com'    => 'whois.verisign-grs.com',
+    'net'    => 'whois.verisign-grs.com',
+    'org'    => 'whois.pir.org',
+    'info'   => 'whois.afilias.net',
+    'cn'     => 'whois.cnnic.cn',
+    'edu.cn' => 'whois.edu.cn',
+    'hk'     => 'whois.hkirc.hk',
+    'tw'     => 'whois.twnic.net.tw',
+    'ai'     => 'whois.nic.ai',
+    'io'     => 'whois.nic.io',
+    'xyz'    => 'whois.nic.xyz',
+    'pp.ua'  => 'whois.ua',
+    // 可继续扩展
+];
